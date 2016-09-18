2bc8efd7e8177e55534ee1ae4d1be937cac4cc7d
Victoria Li <li.victoria51@gmail.com>
2016-09-18 01:56:00 -0400

    adding pasting imaging/text functionality

--- src/main/java/uClip/clipboardChecker.java
+++ src/main/java/uClip/clipboardChecker.java
@@ -5,6 +5,17 @@ import java.awt.datatransfer.*;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+import javax.activation.MimetypesFileTypeMap;
+import javax.imageio.ImageIO;
+
+import java.awt.image.RenderedImage;
+import java.awt.*;
+import java.io.*;
 
 import com.google.firebase.*;
 import com.google.firebase.database.ChildEventListener;
@@ -20,19 +31,47 @@ public class clipboardChecker extends Thread implements ClipboardOwner {
   private FirebaseDatabase db;
   private DatabaseReference ref;
   
+  
   public void run() {
 	    db = FirebaseDatabase.getInstance();
 	    ref = db.getReference("copy"); 
 	    ref.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
+            public void onDataChange(DataSnapshot dataSnapshot){
 	          	String str = dataSnapshot.getValue(String.class);
-            	System.out.println("hi");
-	      		Toolkit toolkit = Toolkit.getDefaultToolkit();
-	      		Clipboard clipboard = toolkit.getSystemClipboard();
-	      		StringSelection strSel = new StringSelection(str);
-	      		clipboard.setContents(strSel, null);
+	          	
+	          	URLConnection connection = null;
+				
+	          	try {
+					connection = new URL(str).openConnection();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} 
+	        	connection.setRequestProperty("User-Agent", "Mozilla/5.0");
+	        	String contentType = connection.getHeaderField("Content-Type"); 
+	          	
+	        	if (contentType.startsWith("image")){
+	        		Image image = null;
+					try {
+						image = ImageIO.read(connection.getInputStream());
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}  
+	            	UrlImagetoPaste.write(image);
+	                javax.swing.ImageIcon icon = new javax.swing.ImageIcon( UrlImagetoPaste.read() );
+	                javax.swing.JLabel label = new javax.swing.JLabel( icon );
+	        	}
+			    
+	        	else {
+			        	Toolkit toolkit = Toolkit.getDefaultToolkit();
+			      		Clipboard clipboard = toolkit.getSystemClipboard();
+			      		StringSelection strSel = new StringSelection(str);
+			      		clipboard.setContents(strSel, null);
+			        }
             }
+	    
 
 			@Override
 			public void onCancelled(DatabaseError arg0) {
@@ -40,21 +79,18 @@ public class clipboardChecker extends Thread implements ClipboardOwner {
 				
 			}
         });
-	  
-
-	   
-	  
+	
     Transferable trans = sysClip.getContents(this);
     regainOwnership(trans);
     System.out.println("Listening to board...");
 	  
     while(true) {}
   }
-  
+ 
   
   public void lostOwnership(Clipboard c, Transferable t) {
 	  try {
-	    this.sleep(2000);
+	    this.sleep(10000);
 	  } catch(Exception e) {
 	    System.out.println("Exception: " + e);
 	  }
@@ -88,7 +124,6 @@ public class clipboardChecker extends Thread implements ClipboardOwner {
   }
   
   
-  
   public static void main(String[] args) {
       try {
           FirebaseOptions options = new FirebaseOptions.Builder()
@@ -102,5 +137,6 @@ public class clipboardChecker extends Thread implements ClipboardOwner {
       }
     clipboardChecker u = new clipboardChecker();
     u.start();
+
   }
 }
